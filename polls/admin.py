a6bc4f846f261a554fbcb006cc074fdf5d9ccce6
Dmitry Potapov <potapov627@yandex.ru>
2024-02-08 00:34:28 +0800

    Change models to another approach

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Choice, Poll, Question, Node
+from .models import Choice, Poll, Question
 
 
 admin.site.register(Choice)
 admin.site.register(Question)
 admin.site.register(Poll)
-admin.site.register(Node)
--- polls/models.py
+++ polls/models.py
@@ -1,57 +1,60 @@
 from django.db import models
-from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
-
-
-class Choice(models.Model):
-    text = models.CharField(max_length=200)
-
-    def __str__(self): 
-        return self.text
-
-
-class Question(models.Model):
-    TYPE = {
-        0: "Custom",
-        1: "Single",
-        2: "Multiple"
-    }
-    text = models.CharField(max_length=800)
-    choice_type = models.PositiveIntegerField(choices=TYPE, default=0)
-    choices = models.ManyToManyField(
-        Choice, related_name='related_polls', blank=True)
-
-    def __str__(self):
-        return self.text
-
-
-class Node(models.Model):
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
-    # limit = models.Q(app_label='polls', model='Question') | models.Q(app_label='polls', model='Choice')
-    node_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    node_object = GenericForeignKey("node_type", "object_id")
-
-    class Meta:
-        indexes = [
-            models.Index(fields=["node_type", "object_id"]),
-        ]
-
-    # def __str__(self):
-    #     return f'{self.node_object.__name__}: {self.node_object.text}'
 
 
 class Poll(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
-    first_node = models.ForeignKey(Node, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
-class Answer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Question(models.Model):
+    TYPE = {
+        0: "Custom",  # Убрать или оставить ответ с текстовым полем без выбора, боюсь запутаться
+        1: "Single",
+        2: "Multiple"
+    }
+    text = models.CharField(max_length=800)
+    choice_type = models.PositiveIntegerField(choices=TYPE, default=0)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    hierarchy_level = models.PositiveIntegerField(default=0)  # Таким образом задаем порядов вопросов в опросе или избыточно ?
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self): 
+        return self.text
+
+
+class Condition(models.Model):
+    TYPE = {
+        0: "Default show",
+        1: "Default hide",  # Не избыточно ли default hide?
+        2: "Hide",
+        3: "Show",
+    }
+    condition_type = models.PositiveIntegerField(choices=TYPE, default=0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+
+class PollResults(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Answer(models.Model):
+    poll_result = models.ForeignKey(PollResults, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+
+
+
